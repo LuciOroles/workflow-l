52a9bf71215969b65ef2cad4b35567561ac90af1
LuciOroles <stoica.florin.lucian@gmail.com>
2016-10-14 13:54:55 +0300

    adding coffescript to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
-var gulp = require('gulp'), gutil =require('gulp-util');
-	gulp.task('log',function (){
-		gutil.log('workflows are shit!');
+var gulp = require('gulp'), 
+	gutil =require('gulp-util')
+	coffee = require('gulp-coffee');
+var coffeSources = ["components/coffe/*.coffe"];
+
+	 
+	gulp.task('coffee',function(){
+		gulp.src(coffeSources)
+		.pipe(coffee({bare:true}))
+		.on('error', gutil.log)
+		.pipe(gulp.dest("components/scripts"));
 	});
